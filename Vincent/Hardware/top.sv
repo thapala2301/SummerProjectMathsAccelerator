97537a1f85882ae88ecaf90ee1c7fff69125b3e3
Vincent <bossardv@gmail.com>
2026-05-30 16:10:02 +0100

    march_core done, top started

---
+++ Vincent/Hardware/top.sv
@@ -0,0 +1,28 @@
+module top(
+    input logic clk,
+    input logic clk_pixel, // HDMI clk
+    input logic rst_n,
+    output logic hdmi_tx_clk_p,
+    output logic hdmi_tx_clk_n,
+    output logic [2:0] hdmi_tx_data_p,
+    output logic [2:0] hdmi_tx_data_n
+);
+
+//hardcoded cam for now
+
+
+//intermed wires
+//px dispatch
+
+//feedback ctrl
+
+//march core
+
+//framebufferbram
+
+//scan out
+
+//rgb2dvi in vivado
+
+
+endmodule
