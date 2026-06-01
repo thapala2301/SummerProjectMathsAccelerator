16bb5dc93b7058c56b286527da8b0c515c3eb95e
Thanus Palakumar <thapala2301@outlook.com>
2026-06-01 12:54:01 +0100

    Reduce sys_clk to 40MHz for timing closure

--- VivadoDesigns/ray_marcher_basev1/ray_marcher_basev1.cache/ip/2020.2/2bf4e3e8e57c6a8c/clk_wiz_0_stub.v
+++ VivadoDesigns/ray_marcher_basev1/ray_marcher_basev1.cache/ip/2020.2/2bf4e3e8e57c6a8c/clk_wiz_0_stub.v
@@ -1,10 +1,10 @@
 // Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2020.2 (win64) Build 3064766 Wed Nov 18 09:12:45 MST 2020
-// Date        : Mon Jun  1 12:43:55 2026
+// Date        : Mon Jun  1 12:43:54 2026
 // Host        : DESKTOP-58PQ9HM running 64-bit major release  (build 9200)
-// Command     : write_verilog -force -mode synth_stub
-//               c:/Users/THAPA/SummerProjectMathsAccelerator/VivadoDesigns/ray_marcher_basev1/ray_marcher_basev1.gen/sources_1/ip/clk_wiz_0/clk_wiz_0_stub.v
+// Command     : write_verilog -force -mode synth_stub -rename_top decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix -prefix
+//               decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix_ clk_wiz_0_stub.v
 // Design      : clk_wiz_0
 // Purpose     : Stub declaration of top-level module interface
 // Device      : xc7z020clg400-1
@@ -13,7 +13,7 @@
 // This empty module with port declaration file causes synthesis tools to infer a black box for IP.
 // The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
 // Please paste the declaration into a Verilog source file or add the file as an additional source.
-module clk_wiz_0(clk_out1, clk_out2, reset, locked, clk_in1)
+module decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix(clk_out1, clk_out2, reset, locked, clk_in1)
 /* synthesis syn_black_box black_box_pad_pin="clk_out1,clk_out2,reset,locked,clk_in1" */;
   output clk_out1;
   output clk_out2;
